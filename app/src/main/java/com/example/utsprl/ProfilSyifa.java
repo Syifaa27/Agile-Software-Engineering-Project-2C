134300bd9ff12c995f110421c5456be56d79ac01
Syifaa27 <syifaauliyaas.15@student.uns.ac.id>
2022-11-02 21:14:16 +0700

    Halaman Profil Syifa

---
+++ app/src/main/java/com/example/utsprl/ProfilSyifa.java
@@ -0,0 +1,14 @@
+package com.example.utsprl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfilSyifa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_syifa);
+    }
+}
