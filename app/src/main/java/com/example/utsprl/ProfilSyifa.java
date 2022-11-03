47094a586027f58e8ee9968f9bacd1c4a062bc8f
Syifaa27 <syifaauliyaas.15@student.uns.ac.id>
2022-11-03 14:09:39 +0700

    Profil Update

--- app/src/main/java/com/example/utsprl/ProfilSyifa.java
+++ app/src/main/java/com/example/utsprl/ProfilSyifa.java
@@ -3,6 +3,12 @@ package com.example.utsprl;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ProfilSyifa extends AppCompatActivity {
 
@@ -10,5 +16,8 @@ public class ProfilSyifa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil_syifa);
+        ListView lv = (ListView) findViewById(R.id.lv);
+
+
     }
 }
